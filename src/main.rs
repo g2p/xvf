e6e7b3885ca1200e605ae31515858823009f99ec
Gabriel <g2p.code@gmail.com>
2017-06-02 18:12:34 +0900

    Extract tar archives

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 #[macro_use]
 extern crate clap;
 
+use std::process::Command;
+
+
 fn main() {
     let matches = clap_app!(myapp =>
         (@arg ARCHIVE: +required ... "Archives to extract")
     ).get_matches();
 
-	for arch in matches.values_of("ARCHIVE").unwrap() {
-		println!("Extract {}", arch);
+    for arch in matches.values_of("ARCHIVE").unwrap() {
+        println!("Extract {}", arch);
+        if arch.ends_with(".tgz") {
+            let status = Command::new("tar")
+                .arg("-xf")
+                .arg(arch)
+                .status()
+                .expect("Failed to launch tar");
+            assert!(status.success(), "tar wasn't successful");
+        } else {
+            println!("Can't handle {}", arch);
+        }
     }
 }
