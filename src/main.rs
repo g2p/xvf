bec1c176249da557e9782ce5907b198ae3a44833
Gabriel de Perthuis <g2p.code@gmail.com>
2022-12-22 11:39:05 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
-#[macro_use]
-extern crate clap;
+extern crate bpaf;
 #[macro_use]
 extern crate lazy_static;
 
 extern crate libc;
 extern crate scmp;
-extern crate tempdir;
+extern crate tempfile;
 
-use std::fs;
+use scmp::*;
 use std::env;
-use std::ffi::OsStr;
 use std::ffi::CString;
+use std::ffi::OsStr;
+use std::fs;
 use std::io::Error;
 use std::os::raw::c_void;
-use std::os::unix::process::CommandExt;
 use std::os::unix::ffi::OsStrExt;
+use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
-use scmp::*;
 
-unsafe fn allow_syscall(ctx : *mut c_void, nr : u32) {
-    assert_eq!(seccomp_rule_add_array(ctx, SCMP_ACT_ALLOW, nr as i32, 0, std::ptr::null()), 0);
+unsafe fn allow_syscall(ctx: *mut c_void, nr: u32) {
+    assert_eq!(
+        seccomp_rule_add_array(ctx, SCMP_ACT_ALLOW, nr as i32, 0, std::ptr::null()),
+        0
+    );
 }
 
 fn setup_seccomp() -> Result<(), Error> {
@@ -37,6 +39,7 @@ fn setup_seccomp() -> Result<(), Error> {
         allow_syscall(ctx, __NR_write);
         allow_syscall(ctx, __NR_stat);
         allow_syscall(ctx, __NR_fstat);
+        allow_syscall(ctx, __NR_newfstatat);
         allow_syscall(ctx, __NR_close);
         allow_syscall(ctx, __NR_mprotect);
         allow_syscall(ctx, __NR_arch_prctl);
@@ -59,18 +62,66 @@ fn setup_seccomp() -> Result<(), Error> {
         allow_syscall(ctx, __NR_wait4);
         allow_syscall(ctx, __NR_exit_group);
         allow_syscall(ctx, __NR_dup);
-        assert_eq!(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, __NR_ioctl as i32, 1, scmp_arg_cmp {
-            arg: 1, op: scmp_compare_SCMP_CMP_EQ, datum_a: libc::FIOCLEX, datum_b: 0,
-        }), 0);
-        assert_eq!(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, __NR_ioctl as i32, 1, scmp_arg_cmp {
-            arg: 1, op: scmp_compare_SCMP_CMP_EQ, datum_a: libc::FIONBIO, datum_b: 0,
-        }), 0);
-        assert_eq!(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, __NR_ioctl as i32, 1, scmp_arg_cmp {
-            arg: 1, op: scmp_compare_SCMP_CMP_EQ, datum_a: libc::TCGETS, datum_b: 0,
-        }), 0);
-        assert_eq!(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, __NR_ioctl as i32, 1, scmp_arg_cmp {
-            arg: 1, op: scmp_compare_SCMP_CMP_EQ, datum_a: libc::TIOCGWINSZ, datum_b: 0,
-        }), 0);
+        assert_eq!(
+            seccomp_rule_add(
+                ctx,
+                SCMP_ACT_ALLOW,
+                __NR_ioctl as i32,
+                1,
+                scmp_arg_cmp {
+                    arg: 1,
+                    op: scmp_compare_SCMP_CMP_EQ,
+                    datum_a: libc::FIOCLEX,
+                    datum_b: 0,
+                }
+            ),
+            0
+        );
+        assert_eq!(
+            seccomp_rule_add(
+                ctx,
+                SCMP_ACT_ALLOW,
+                __NR_ioctl as i32,
+                1,
+                scmp_arg_cmp {
+                    arg: 1,
+                    op: scmp_compare_SCMP_CMP_EQ,
+                    datum_a: libc::FIONBIO,
+                    datum_b: 0,
+                }
+            ),
+            0
+        );
+        assert_eq!(
+            seccomp_rule_add(
+                ctx,
+                SCMP_ACT_ALLOW,
+                __NR_ioctl as i32,
+                1,
+                scmp_arg_cmp {
+                    arg: 1,
+                    op: scmp_compare_SCMP_CMP_EQ,
+                    datum_a: libc::TCGETS,
+                    datum_b: 0,
+                }
+            ),
+            0
+        );
+        assert_eq!(
+            seccomp_rule_add(
+                ctx,
+                SCMP_ACT_ALLOW,
+                __NR_ioctl as i32,
+                1,
+                scmp_arg_cmp {
+                    arg: 1,
+                    op: scmp_compare_SCMP_CMP_EQ,
+                    datum_a: libc::TIOCGWINSZ,
+                    datum_b: 0,
+                }
+            ),
+            0
+        );
         allow_syscall(ctx, __NR_getpid);
         allow_syscall(ctx, __NR_gettid);
         allow_syscall(ctx, __NR_tgkill);
@@ -105,15 +156,24 @@ fn setup_seccomp() -> Result<(), Error> {
     Ok(())
 }
 
-const RENAME_NOREPLACE : u64 = 1;
+const RENAME_NOREPLACE: u64 = 1;
 
-fn path_to_cstring(path : &Path) -> CString {
+fn path_to_cstring(path: &Path) -> CString {
     CString::new(path.as_os_str().as_bytes()).unwrap()
 }
 
-fn rename_noreplace(path0 : &Path, path1 : &Path) -> Result<(), Error> {
+fn rename_noreplace(path0: &Path, path1: &Path) -> Result<(), Error> {
     unsafe {
-        if libc::syscall(__NR_renameat2 as i64, libc::AT_FDCWD, path_to_cstring(path0).as_ptr(), libc::AT_FDCWD, path_to_cstring(path1).as_ptr(), RENAME_NOREPLACE, 0) == 0 {
+        if libc::syscall(
+            __NR_renameat2 as i64,
+            libc::AT_FDCWD,
+            path_to_cstring(path0).as_ptr(),
+            libc::AT_FDCWD,
+            path_to_cstring(path1).as_ptr(),
+            RENAME_NOREPLACE,
+            0,
+        ) == 0
+        {
             Ok(())
         } else {
             Err(Error::last_os_error())
@@ -121,7 +181,7 @@ fn rename_noreplace(path0 : &Path, path1 : &Path) -> Result<(), Error> {
     }
 }
 
-fn rename_or_suffix(path0 : &Path, path1 : &Path) -> Result<PathBuf, Error> {
+fn rename_or_suffix(path0: &Path, path1: &Path) -> Result<PathBuf, Error> {
     let rv = rename_noreplace(path0, path1);
     if let Err(err) = rv {
         if err.raw_os_error().expect("Error fails to wrap errno") != libc::EEXIST {
@@ -145,14 +205,13 @@ fn rename_or_suffix(path0 : &Path, path1 : &Path) -> Result<PathBuf, Error> {
     }
 }
 
-
 struct ArchiveType {
     extensions: Vec<&'static str>,
     extract_cmd: Vec<&'static str>,
 }
 
 lazy_static! {
-    static ref ARCHIVE_TYPES : Vec<ArchiveType> = vec![
+    static ref ARCHIVE_TYPES: Vec<ArchiveType> = vec![
         ArchiveType {
             extensions: vec![
                 ".tgz",
@@ -169,36 +228,30 @@ lazy_static! {
             extract_cmd: vec!["tar", "-xf"],
         },
         ArchiveType {
-            extensions: vec![
-                ".zip",
-            ],
+            extensions: vec![".zip",],
             extract_cmd: vec!["unzip", "--"],
         },
         ArchiveType {
-            extensions: vec![
-                ".part1.rar",
-                ".rar",
-            ],
+            extensions: vec![".part1.rar", ".rar",],
             extract_cmd: vec!["unrar", "x", "--"],
         },
         ArchiveType {
-            extensions: vec![
-                ".7z",
-            ],
+            extensions: vec![".7z",],
             extract_cmd: vec!["7z", "x", "--"],
         },
     ];
 }
 
-
 fn main() {
-    let matches = clap_app!(myapp =>
-        (@arg ARCHIVE: +required ... "Archives to extract")
-    ).get_matches();
+    let parser = bpaf::positional("ARCHIVE").some("At least one archive is required");
+    let archives = bpaf::Info::default()
+        .descr("Extract archives")
+        .for_parser(parser)
+        .run();
 
     let mut found = false;
 
-    for arch in matches.values_of_os("ARCHIVE").unwrap() {
+    for arch in &archives {
         let arch = Path::new(arch);
         let arch_bytes = arch.as_os_str().as_bytes();
         println!("Extract {:?}", arch);
@@ -210,37 +263,44 @@ fn main() {
                 }
                 found = true;
 
-                let noext : &OsStr = OsStrExt::from_bytes(&arch_bytes[0..arch_bytes.len()-ext_bytes.len()]);
-                let stripped =
-                    if let Some(noext) = Path::new(noext).file_name() {
-                        noext
-                    } else {
-                        OsStr::new("extracted")
-                    };
+                let noext: &OsStr =
+                    OsStrExt::from_bytes(&arch_bytes[0..arch_bytes.len() - ext_bytes.len()]);
+                let stripped = if let Some(noext) = Path::new(noext).file_name() {
+                    noext
+                } else {
+                    OsStr::new("extracted")
+                };
                 let cmd = at.extract_cmd[0];
-                let tmpdir = tempdir::TempDir::new_in(
-                    env::current_dir().expect("Unable to get the current directory"),
-                    stripped.to_string_lossy().to_mut().as_str()).expect("Unable to create temporary directory");
-                let status = unsafe { Command::new(cmd)
-                    .args(&at.extract_cmd[1..])
-                    .arg(fs::canonicalize(&arch).expect("Unable to find archive"))
-                    .current_dir(&tmpdir)
-                    .pre_exec(setup_seccomp)
-                    .status()
-                    .unwrap_or_else(|_| panic!("Failed to launch {}", cmd))};
-                assert!(status.success(), format!("{} wasn't successful", cmd));
+                let tmpdir = tempfile::Builder::new()
+                    .prefix(stripped)
+                    .tempdir_in(env::current_dir().expect("Unable to get the current directory"))
+                    .expect("Unable to create temporary directory");
+                let status = unsafe {
+                    Command::new(cmd)
+                        .args(&at.extract_cmd[1..])
+                        .arg(fs::canonicalize(arch).expect("Unable to find archive"))
+                        .current_dir(&tmpdir)
+                        .pre_exec(setup_seccomp)
+                        .status()
+                        .unwrap_or_else(|_| panic!("Failed to launch {}", cmd))
+                };
+                assert!(status.success(), "{} wasn't successful", cmd);
                 let extracted_path = tmpdir.into_path();
-                let mut dir_list = fs::read_dir(&extracted_path).expect("Unable to read the extraction directory");
+                let mut dir_list =
+                    fs::read_dir(&extracted_path).expect("Unable to read the extraction directory");
                 if let Some(path) = dir_list.next() {
                     let path = path.expect("Unable to iterate the extraction directory");
                     if let Some(path1) = dir_list.next() {
                         path1.expect("Unable to iterate the extraction directory");
-                        let newname = rename_or_suffix(&extracted_path, Path::new(stripped)).expect("Failed to rename");
+                        let newname = rename_or_suffix(&extracted_path, Path::new(stripped))
+                            .expect("Failed to rename");
                         println!("Extracted to {:?}", newname);
                     } else {
-                        let newname = rename_or_suffix(&path.path(), Path::new(stripped)).expect("Failed to rename");
+                        let newname = rename_or_suffix(&path.path(), Path::new(stripped))
+                            .expect("Failed to rename");
                         println!("Extracted to {:?}", newname);
-                        fs::remove_dir(&extracted_path).expect("Failed to remove empty extraction directory");
+                        fs::remove_dir(&extracted_path)
+                            .expect("Failed to remove empty extraction directory");
                     }
                 } else {
                     println!("No files were extracted");
@@ -253,4 +313,3 @@ fn main() {
         }
     }
 }
-
